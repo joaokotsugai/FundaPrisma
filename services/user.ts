4a4f31a7cd6d2e2b63d1eb2a3061022007c4db32
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-05-04 08:37:45 -0300

    modificando codico

--- services/user.ts
+++ services/user.ts
@@ -1,13 +1,18 @@
 import { Prisma } from '@prisma/client'
 import { prisma } from "../src/libs/prisma"
 
-export const createUser = async (name: string, email: string) => {
+type CreateUserProps = {
+    name: string
+    email: string
+}
+
+export const createUser = async ({name,  email}: CreateUserProps) => {
     const user = await prisma.user.create({
         data: {
             name,
             email
         }
-
     })
+
     return user
 }
