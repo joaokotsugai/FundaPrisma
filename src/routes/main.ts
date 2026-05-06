bdf54d6eab25941400b9947642d0beb5bfd6353b
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-05-06 08:35:31 -0300

    evitando erros

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
-import { prisma } from '../libs/prisma';
+import { createUser } from '../services/user';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
-res.json({ pong: true });
+    res.json({ pong: true });
 });
 
 mainRouter.post('/user', async (req, res) => {
     const user = await createUser({
         name: 'John Doe',
         email: 'john.doe@exemple.com'
-});
-
-res.json(user);
+    });
+    if (user) {
+        res.status(201).json({ user });
+    } else {
+        res.status(400).json({ error: "Email already exists" });
+    }
+    res.json(user);
 });
