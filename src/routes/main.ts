cf3e4fd4c0d901a78a831090badd81654454590a
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-05-13 08:37:38 -0300

    listando usuarios

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma';
-import { createUser, createUsers } from '../services/user';
+import { createUser, createUsers, getAllUsers } from '../services/user';
+import { get } from 'http';
+
 
 
 export const mainRouter = Router();
@@ -11,8 +13,14 @@ mainRouter.get('/ping', (req, res) => {
 
 mainRouter.post('/user', async (req, res) => {
     const user = await createUser({
-        name: 'John Doe',
-        email: 'john.doe@exemple.com'
+        name: 'Wild Bill',
+        email: 'wild.bill@exemple.com',
+        posts: {
+            create: {
+                title: 'Post 1 - Wild Bill',
+                content: 'Content of post 1 - Wild Bill',
+            }
+        }
     });
     if (user) {
         res.status(201).json({ user });
@@ -32,5 +40,14 @@ mainRouter.post('/users', async (req, res) => {
         res.status(201).json({ ok: true})
     } else {
         res.status(400).json({ error: "Error creating users" })
+    }
+})
+
+mainRouter.get('/users' , async (req, res) => {
+    const users = await getAllUsers()
+    if (users) {
+        res.json({ users })
+    } else {
+        res.status(500).json({ error: "Error fetching users" })
     }
 })
